60125a45b561a8a574d0d153e6c0acfca25a4f8d
Ayush Jain <ayush.jain@johnlewis.co.uk>
2025-03-19 19:02:36 +0530

    Fixed the ID Generation Type

--- src/main/java/com/learning/shoppingcart/Entity/ProductDetails.java
+++ src/main/java/com/learning/shoppingcart/Entity/ProductDetails.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
 public class ProductDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String description;
